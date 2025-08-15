52dd54430af9e5d046e71026f1f5a7dca28ccdfa
blankMCU <vexown@gmail.com>
2025-08-15 22:02:21 +0200

    Reduce task period for QMI8658 and AK09918 data reading to 50 ms for improved responsiveness

--- ESP32/Application/components/IMU/Source/IMU.cpp
+++ ESP32/Application/components/IMU/Source/IMU.cpp
@@ -139,10 +139,10 @@
 /* FreeRTOS task configuration */
 #define TASK_READ_QMI8658_DATA_PRIORITY (tskIDLE_PRIORITY + 1)
 #define TASK_READ_QMI8658_DATA_STACK_SIZE 4096 // Stack size in bytes
-#define TASK_READ_QMI8658_DATA_PERIOD_TICKS pdMS_TO_TICKS(100) // Task period in ticks (100 ms)
+#define TASK_READ_QMI8658_DATA_PERIOD_TICKS pdMS_TO_TICKS(50) // Task period in ticks (50 ms)
 #define TASK_READ_AK09918_DATA_PRIORITY (tskIDLE_PRIORITY + 1)
 #define TASK_READ_AK09918_DATA_STACK_SIZE 4096 // Stack size in bytes
-#define TASK_READ_AK09918_DATA_PERIOD_TICKS pdMS_TO_TICKS(100) // Task period in ticks (100 ms)
+#define TASK_READ_AK09918_DATA_PERIOD_TICKS pdMS_TO_TICKS(50) // Task period in ticks (50 ms)
 
 /*******************************************************************************/
 /*                                DATA TYPES                                   */
